b6bc8e3078b6afb0842804bd54e4bd7702888e3f
qwerty2501 <qwerty2501@live.jp>
2020-12-09 16:39:04 +0900

    判定式修正

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ set -eu
 wd=$(dirname $0)
 cd $wd
 
-if [ "$(uname)" = "Darwin" ] && [ "$(command -v brew)" = "" ]; then
+if [ "$(uname)" = "Darwin" ] && ! command -v brew 2>&1; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [ "$(command -v apt)" != "" ]; then
+if command -v apt 2>&1; then
 	sudo apt install ansible
-elif [ "$(uname)" = "Darwin" ]; then
+elif command -v brew 2>&1; then
 	brew install ansible
+else
+	echo "対応していない環境です"
 fi
