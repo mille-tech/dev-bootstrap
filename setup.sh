2006fd405286be3b3f67731b70a10f13d0e41e9a
qwerty2501 <qwerty2501@live.jp>
2020-12-09 18:18:12 +0900

    ansible実行処理追加

--- setup.sh
+++ setup.sh
@@ -5,13 +5,19 @@ wd=$(dirname $0)
 cd $wd
 
 if [ "$(uname)" = "Darwin" ] && ! command -v brew 2>&1; then
+	echo "brewをインストール"
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+echo "ansibleをインストール"
 if command -v apt 2>&1; then
 	sudo apt install ansible
 elif command -v brew 2>&1; then
 	brew install ansible
 else
 	echo "対応していない環境です"
+	exit 1
 fi
+
+ansible-galaxy install cimon-io.asdf
+ansible-playbook ansible/setup.yaml --ask-become-pass
