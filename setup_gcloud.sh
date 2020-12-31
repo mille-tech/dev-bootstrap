47259bf618e068afcf055fbeb7bcc704fd9adef1
qwerty2501 <qwerty2501@live.jp>
2020-12-31 14:40:12 +0900

    chore: スクリプトの役割を分割した
    情報を環境変数ではなく引数で渡すようにした

--- setup_gcloud.sh
+++ setup_gcloud.sh
@@ -2,16 +2,24 @@
 
 set -eu
 
+
+console_only=""
+no_launch_browser=""
+for OPT in "$@"
+do
+	case $OPT in
+		--no-browser)
+			console_only="--console-only"
+			no_launch_browser="--no-launch-browser"
+			shift 1;;
+	esac
+done
+
+$HOME/google-cloud-sdk/bin/gcloud init $console_only
+$HOME/google-cloud-sdk/bin/gcloud auth login $no_launch_browser
+$HOME/google-cloud-sdk/bin/gcloud auth application-default login $no_launch_browser
+$HOME/google-cloud-sdk/bin/gcloud auth configure-docker --quiet
+
 if [ "$require_google_auth" = "true" ]; then
-	console_only=""
-	no_launch_browser=""
-	if [ "$on_wsl" = "true" ]; then
-		console_only="--console-only"
-		no_launch_browser="--no-launch-browser"
-	fi
-	$HOME/google-cloud-sdk/bin/gcloud init $console_only
-	$HOME/google-cloud-sdk/bin/gcloud auth login $no_launch_browser
-	$HOME/google-cloud-sdk/bin/gcloud auth application-default login $no_launch_browser
-	$HOME/google-cloud-sdk/bin/gcloud auth configure-docker --quiet
 
 fi
